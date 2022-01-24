ad80409142903406f6406edf9cdf11977c4adae9
TomPzt <98309277+TomPzt@users.noreply.github.com>
2022-01-24 09:15:19 +0100

    Add files via upload

---
+++ CalculsSimples.py
@@ -0,0 +1,23 @@
+def addition(a, b):
+    """Calcul la somme des deux nombres passés en paramètre
+
+    Parameters:
+        a: float, premier nombre à additionner
+        b: float, deuxième nombre à additionner
+
+    Returns:
+        Résultat de la somme
+    """
+    return a+b
+
+def soustraction(a, b):
+    """Calcul la différence des deux nombres passés en paramètre
+
+    Parameters:
+        a: float, nombre de base
+        b: float, nombre à soustraire
+
+    Returns:
+        Résultat de la somme
+    """
+    return a-b
